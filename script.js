8fbd46fd932414c7bf8e95222312df27ff001b70
Saran Kumar S <110930872+Naras23@users.noreply.github.com>
2022-09-15 13:02:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+setTimeout(() => {
+  var num8 = document.getElementById('nums');
+  num8.innerHTML = '8';
+  setTimeout(() => {
+    var num7 = document.getElementById('nums');
+    num7.innerHTML = '7';
+    setTimeout(() => {
+      var num6 = document.getElementById('nums');
+      num6.innerHTML = '6';
+      setTimeout(() => {
+        var num5 = document.getElementById('nums');
+        num5.innerHTML = '5';
+        setTimeout(() => {
+          var num4 = document.getElementById('nums');
+          num4.innerHTML = '4';
+          setTimeout(() => {
+            var num3 = document.getElementById('nums');
+            num3.innerHTML = '3';
+            setTimeout(() => {
+              var num2 = document.getElementById('nums');
+              num2.innerHTML = '2';
+              setTimeout(() => {
+                var num1 = document.getElementById('nums');
+                num1.innerHTML = '1';
+                setTimeout(() => {
+                  var num1 = document.getElementById('nums');
+                  num1.innerHTML = "HAPPY ENGINEER'S DAY";
+                }, 1000);
+              }, 1000);
+            }, 1000);
+          }, 1000);
+        }, 1000);
+      }, 1000);
+    }, 1000);
+  }, 1000);
+}, 1000);
